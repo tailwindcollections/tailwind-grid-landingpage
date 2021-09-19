c48033e7427b31814bf64f9540c26d9efa6c3420
Misbah Ansori <misbah.ansori24@gmail.com>
2021-09-19 10:28:12 +0700

    enable hmr

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import svgLoader from 'vite-svg-loader'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
-  // Disable auto-reload for screencast only
-  server : {
-    hmr : false,
-  }
 })
